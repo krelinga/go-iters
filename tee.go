f82646437df893bce0ce6245b22e9deee3414cbf
Andy Kreling <krelinga@gmail.com>
2025-06-06 22:13:53 +0000

    Tee().

---
+++ tee.go
@@ -0,0 +1,46 @@
+package iters
+
+import "iter"
+
+// Tee copies values from the input sequence to the given output sinks before yielding them to the returned iterator.
+func Tee[T any](seq iter.Seq[T], sinks ...Sink[T]) iter.Seq[T] {
+	if len(sinks) == 0 {
+		return seq
+	}
+	return func(yield func(T) bool) {
+		closedCount := 0
+		closed := make([]bool, len(sinks))
+		var retClosed bool
+
+		tryWrite := func(sink Sink[T], val T, closed *bool) {
+			if *closed {
+				return
+			}
+			if !sink.Write(val) {
+				sink.Close()
+				*closed = true
+				closedCount++
+			}
+		}
+
+		for val := range seq {
+			if closedCount == len(sinks)+1 {
+				break // All sinks and default output are closed, exit the loop
+			}
+			for i, sink := range sinks {
+				tryWrite(sink, val, &closed[i])
+			}
+			if !retClosed && !yield(val) {
+				retClosed = true
+				closedCount++
+				break
+			}
+		}
+
+		for i, sink := range sinks {
+			if !closed[i] {
+				sink.Close()
+			}
+		}
+	}
+}
