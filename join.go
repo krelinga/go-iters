7eb636eccbf81ae830607d29cc12fc498912400e
Andy Kreling <krelinga@gmail.com>
2025-06-06 21:25:46 +0000

    add JoinPadWith().
    further remove parallel processing comments.

--- join.go
+++ join.go
@@ -4,24 +4,33 @@ import "iter"
 
 // JoinPad combines two `iter.Seq`s into a single `iter.Seq2`.
 // It stops when the longer sequence is exhausted, padding the shorter one with zero values.
-// It consumes elements from both sequences in parallel.
 func JoinPad[T1, T2 any](one iter.Seq[T1], two iter.Seq[T2]) iter.Seq2[T1, T2] {
-	return joinImpl(one, two, func(oneOk, twoOk bool) bool {
+	var onePad T1
+	var twoPad T2
+	return JoinPadWith(one, two, onePad, twoPad)
+}
+
+// JoinPadWith combines two `iter.Seq`s into a single `iter.Seq2`.
+// It stops when the longer sequence is exhausted, padding the shorter one with the provided pad value.
+func JoinPadWith[T1, T2 any](one iter.Seq[T1], two iter.Seq[T2], onePad T1, twoPad T2) iter.Seq2[T1, T2] {
+	return joinImpl(one, two, onePad, twoPad, func(oneOk, twoOk bool) bool {
 		return !oneOk && !twoOk // Stop when both sequences are exhausted
 	})
 }
 
 // JoinTrim combines two `iter.Seq`s into a single `iter.Seq2`.
 // It stops when the shorter sequence is exhausted, ignoring any remaining elements in the longer sequence.
-// It consumes elements from both sequences in parallel.
 func JoinTrim[T1, T2 any](one iter.Seq[T1], two iter.Seq[T2]) iter.Seq2[T1, T2] {
-	return joinImpl(one, two, func(oneOk, twoOk bool) bool {
+	// Not really necesary since we are trimming, but needed to call joinImpl.
+	var onePad T1
+	var twoPad T2
+	return joinImpl(one, two, onePad, twoPad, func(oneOk, twoOk bool) bool {
 		return !oneOk || !twoOk // Stop when either sequence is exhausted
 	})
 }
 
-func joinImpl[T1, T2 any](one iter.Seq[T1], two iter.Seq[T2], stopWhen func(bool, bool) bool) iter.Seq2[T1, T2] {
-	return func(yield func (T1, T2) bool) {
+func joinImpl[T1, T2 any](one iter.Seq[T1], two iter.Seq[T2], onePad T1, twoPad T2, stopWhen func(bool, bool) bool) iter.Seq2[T1, T2] {
+	return func(yield func(T1, T2) bool) {
 		oneGet, oneDone := iter.Pull(one)
 		twoGet, twoDone := iter.Pull(two)
 		defer oneDone()
