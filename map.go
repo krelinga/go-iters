19461750fd48ffc69df548d91fb7f6f0559f741e
Andy Kreling <krelinga@gmail.com>
2025-06-09 22:16:07 +0000

    add Map2().

--- map.go
+++ map.go
@@ -12,4 +12,12 @@ func Map[T, R any](seq iter.Seq[T], fn func(T) R) iter.Seq[R] {
 	}
 }
 
-// TODO: add Map2 and MapBreak/MapBreak2.
+func Map2[T1, T2, R1, R2 any](seq iter.Seq2[T1, T2], fn func(T1, T2) (R1, R2)) iter.Seq2[R1, R2] {
+	return func(yield func(R1, R2) bool) {
+		for one, two := range seq {
+			if !yield(fn(one, two)) {
+				return
+			}
+		}
+	}
+}
